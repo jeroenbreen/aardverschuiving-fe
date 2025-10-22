b97aabf91f99ba00099a242f179a4d827de2b009
Jeroen Breen <jeroen@innouveau.nl>
2025-10-22 15:07:40 +0200

    Update main.ts

--- frontend/src/stores/main.ts
+++ frontend/src/stores/main.ts
@@ -32,7 +32,7 @@ export const useMainStore = defineStore("main", {
             partyLib: {},
             distances: [],
             grid: 30,
-            threshold: 100 * 1000,
+            threshold: 0 * 1000,
             // the numbers based on ranking
         } as MainState;
     },
